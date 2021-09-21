a85f01b324e1449cb71079d128a3c6666e4a9730
DatOneGuyy <computingpotatochips@gmail.com>
2021-09-21 08:56:58 -0700

    Update competition_0.1.cpp

--- competition/competition_0.1/competition_0.1.cpp
+++ competition/competition_0.1/competition_0.1.cpp
@@ -3,10 +3,10 @@
 // [Name]               [Type]        [Port(s)]
 // Controller1          controller                    
 // inertial5            inertial      5               
-// front_left           motor         1               
-// front_right          motor         3               
-// back_left            motor         2               
-// back_right           motor         4               
+// front_left           motor         11              
+// front_right          motor         13              
+// back_left            motor         12              
+// back_right           motor         14              
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -25,8 +25,29 @@ void autonomous() {
 
 }
 
-void user_control() {
+int left_speed = 0;
+int right_speed = 0;
+double drive_kp = 0.3;
+double drive_kd = 0.05;
+int drive_error = 0;
+int previous_drive_error;
+int drive_derivative = 0;
 
+void user_control() {
+  left_speed = Controller1.Axis1.position(pct);
+  right_speed = Controller1.Axis3.position(pct);
+  if (std::abs(Controller1.Axis1.position(pct) - Controller1.Axis3.position(pct)) < 5) {
+    drive_error = left_speed - right_speed;
+    drive_derivative = previous_drive_error - drive_error;
+    previous_drive_error = drive_error;
+    drive_derivative = 
+    right_speed = left_speed;
+}
+
+  front_left.spin(fwd, map((double)Controller1.Axis1.position(pct)), pct);
+  back_left.spin(fwd, map((double)Controller1.Axis1.position(pct)), pct);
+  front_right.spin(fwd, map((double)Controller1.Axis3.position(pct)), pct);
+  back_right.spin(fwd, map((double)Controller1.Axis3.position(pct)), pct);
 }
 
 int main() {
@@ -36,6 +57,8 @@ int main() {
   pre_auton();
 
   while(1) {
-    wait(100, msec);
+
+    user_control();
+
   }
 }
